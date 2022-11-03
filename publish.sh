0dbde9ddb4f048c3618e3528468cb444b0c71e2d
Orellabac <orellabac@gmail.com>
2022-11-03 18:53:57 +0000

    qualify stage

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+echo "PUBLISHING to $SNOWSQL_WORK_DATABASE"
+
 ~/bin/snowsql -o quiet=true <<QUERY
 use database $SNOWSQL_WORK_DATABASE;
 
@@ -5,11 +7,11 @@ create stage if not exists workstage;
 put file://./target/scala-2.12/ci-cd-sample-assembly-1.0-$GITHUB_REF_NAME.jar @workstage overwrite=true  auto_compress=FALSE;
 
 
-create or replace stage pdf_external
+create or replace stage public.pdf_external
 url="s3://sfquickstarts/Analyze PDF Invoices/Invoices/"
 directory = (enable = TRUE);
 
-create or replace function extract_pdf_text(file string)
+create or replace function public.extract_pdf_text(file string)
 returns String
 language java
 imports = ('@workstage/ci-cd-sample-assembly-1.0-$GITHUB_REF_NAME.jar')
